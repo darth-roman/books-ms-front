0747b94058af1f7d993544b83cb374252f02784d
Chouchane Roman <chouchane.louai20@gmail.com>
2025-01-03 23:48:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+  <!-- nav component -->
+  <nav>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">Books</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+    </ul>    
+  </nav>
+
+  <!-- Form Component -->
+  <form>
+    <label for="search">
+      Look for a Book
+      <input type="text" name="search" id="search" class="search">
+    </label>
+  </form>
+  <!-- Filters Component -->
+  <div class="filters"></div>
+
+</template>
+
+<style scoped>
+</style>
